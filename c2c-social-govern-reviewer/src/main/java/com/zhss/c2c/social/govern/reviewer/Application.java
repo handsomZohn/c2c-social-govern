6e296e64d1012fbcf12076515c74f13eb2f1c9fb
zohn <zzohn@foxmail.com>
2021-02-07 17:52:29 +0800

    c2c-social-govern

---
+++ c2c-social-govern-reviewer/src/main/java/com/zhss/c2c/social/govern/reviewer/Application.java
@@ -0,0 +1,19 @@
+package com.zhss.c2c.social.govern.reviewer;
+
+import com.zhss.c2c.social.govern.reviewer.db.DruidDataSourceConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@Import(DruidDataSourceConfig.class)
+public class Application {
+
+    // 启动服务
+    // 扫描注解 根据配置扫描 扫描包下有@Service 发布这些服务 监听20880端口 找到注册中心 注册进去
+    public static void main(String[] args) {
+        SpringApplication.run(com.zhss.c2c.social.govern.reviewer.Application.class, args);
+    }
+
+}
+
---
+++ c2c-social-govern-reviewer/src/main/java/com/zhss/c2c/social/govern/reviewer/service/ReviewerServiceImpl.java
@@ -0,0 +1,77 @@
+package com.zhss.c2c.social.govern.reviewer.service;
+
+import com.zhss.c2c.social.govern.reviewer.api.ReviewerService;
+import com.zhss.c2c.social.govern.reviewer.dao.ReviewerTaskStatusDAO;
+import com.zhss.c2c.social.govern.reviewer.domain.ReviewerTaskStatus;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *@Description 评审员service
+ *@CreateDate 2021\2\7 0007 13:59
+ *@Author z
+ *@Version 1.0
+ */
+@Service(
+        version = "1.0.0",
+        // 接口类型
+        interfaceClass = ReviewerService.class,
+        // 集群故障转移策略
+        cluster = "failfast",
+        // 负载均衡算法
+        loadbalance = "roundrobin"
+)
+public class ReviewerServiceImpl implements ReviewerService  {
+
+
+    /**
+     * 评审员处理任务DAO组件
+     */
+    @Autowired
+    private ReviewerTaskStatusDAO reviewerTaskStatusDAO;
+
+    /**
+     * 选择评审员
+     * @param reportTaskId 举报任务id
+     * @return 评审员用户id
+     */
+    @Override
+    public List<Long> selectReviewers(Long reportTaskId) {
+        // 模拟通过算法选择一批评审员
+        System.out.println("模拟通过算法选择一批评审员");
+
+        List<Long> reviewerIds = new ArrayList<>();
+        reviewerIds.add(1L);
+        reviewerIds.add(2L);
+        reviewerIds.add(3L);
+        reviewerIds.add(4L);
+        reviewerIds.add(5L);
+
+        // 把每个评审员要执行的任务录入数据库
+        for(Long reviewerId : reviewerIds) {
+            ReviewerTaskStatus reviewerTaskStatus = new ReviewerTaskStatus();
+            reviewerTaskStatus.setReviewerId(reviewerId);
+            reviewerTaskStatus.setReportTaskId(reportTaskId);
+            reviewerTaskStatus.setStatus(ReviewerTaskStatus.PROCESSING);
+            reviewerTaskStatusDAO.add(reviewerTaskStatus);
+        }
+        return reviewerIds;
+    }
+
+    /**
+     * 完成投票
+     * @param reviewerId 评审员id
+     * @param reportTaskId 举报任务id
+     */
+    @Override
+    public void finishVote(Long reviewerId, Long reportTaskId) {
+        ReviewerTaskStatus reviewerTaskStatus = new ReviewerTaskStatus();
+        reviewerTaskStatus.setReviewerId(reviewerId);
+        reviewerTaskStatus.setReportTaskId(reportTaskId);
+        reviewerTaskStatus.setStatus(ReviewerTaskStatus.FINISHED);
+        reviewerTaskStatusDAO.update(reviewerTaskStatus);
+    }
+}
